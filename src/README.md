a565a1008466e0d3b09ad92b8044c59e8f774d59
ComeDownToUs <blues4ceausescu@gmail.com>
2018-03-28 23:59:09 +0100

    aggregated functions fully modular, readme updated

--- src/README.md
+++ src/README.md
@@ -1,4 +1,28 @@
-I'm going to try and make this into something I can break off from the other project.
+This is broken off from a portion of a coding challenge I received where there were issues with the Nextbus API request limit that seemed unnecessary.
+
+The basic goal is to generate a selection of tools which will perform requests to Nextbus and build new data models from them to refer to for either offline use or to reduce the size of requests required.
+These will deal with static data relating to routes, stops and (hopefully) schedules
+
+Aims:
+
+- Schema documentation
+- GeoJSONs of anything I possibly can
+- Modularity
+- Switch to generic Node API requests, no dependencies
+- Validation
+- Verify other agencies work
+- Testing
+- Scan schedules to flag limited routes (e.g. night, weekends/weekdays only), and 24 hour routes
+
+-----
+Old Notes:
+
+Deduced infomation from API results (Note: may be SF-Muni exclusive)
+
+- five digit tags represent duplicates of three or four digit ones, same name and coordinates (e.g. 4th St & Castro St)
+- rail, tram and bus services provided by the same operation may not be differentiated even if they potentially are priced differently
+- there's going to be a lot of restrictions as to what kind of metadata can be derived from schedules without being extremely extensive (e.g. there are a lot of infrequent used outlier stops on routes which prevent the reliabilitiy of which stops are served on a given route). May still include something like "last outbound train" and "last outbound train to end of route" as a proof of concept.
+
 
 Initialisation:
 The apiCalls.js script's outputs (see routeData and schedules directories) are included here already due to being unstable at the moment regarding request limits. If they haven't been updated in a while you may want to run that script yourself and overwrite them, but please keep track of error messages being returned.
@@ -6,20 +30,3 @@ The apiCalls.js script's outputs (see routeData and schedules directories) are i
 Following that, run the following from this directory
 - `node buildAggregatedData.js` will generate shortened versions of the routes info and derive stop data from the routes info too. There's quite a huge amount of duplicate data in the form of stop information in the direct api requests regarding route details, by pulling everything into our own files and only including reference values for stops, the API should be significantly harder to drain
 - `node buildGeoJsons.js` will generate all of the routes along with relevant properties as a features collection in a geojson file, you can check this out by plugging the data into geojson.io to verify, due to the sheer number of stops (easily in excess of 1000 in San Francisco), this full file may not be that useful
-
-
-Aims:
-- Schema document
-- Switch to generic Node API requests, no dependencies
-- Better handling of hitting API request limits (perhaps force users to build in bursts of 20 routes at a time with no file writing until successful)
-- Scan schedules to flag limited routes (e.g. night, weekends/weekdays only), and 24 hour routes
-- Validation and validation testing
-- Docopt interaction or something along those lines for independent usage (alternatively, develop into an npm package)
-- This is getting extensive enough now that testing is really required for some of the manipulations I have in mind
-
-
-Deduced infomation from API results (Note: may be SF-Muni exclusive)
-- five digit tags represent duplicates of three or four digit ones, same name and coordinates (e.g. 4th St & Castro St)
-- rail, tram and bus services provided by the same operation may not be differentiated even if they potentially are priced differently
-- there's going to be a lot of restrictions as to what kind of metadata can be derived from schedules without being extremely extensive (e.g. there are a lot of infrequent used outlier stops on routes which prevent the reliabilitiy of which stops are served on a given route). May still include something like "last outbound train" and "last outbound train to end of route" as a proof of concept.
-
--- src/buildAggregatedData.js
+++ src/buildAggregatedData.js
@@ -4,10 +4,6 @@ var jsonIO = require("./jsonIO.js");
 var readJSON = jsonIO.readJSON;
 var writeJSON = jsonIO.writeJSON;
 
-var dir = "src/output";
-var routeDataDir = dir + "/routeData";
-var aggregatedDir = dir + "/aggregatedData";
-
 function parseCoordinates(coordinates) {
   return [parseFloat(coordinates[0]), parseFloat(coordinates[1])];
 };
@@ -29,7 +25,7 @@ function addNewStops(routeStops, aggregator) {
       duplicateCounter += 1;
     }
   });
-  // console.log("Number of duplicate stops ommitted: " + duplicateCounter);
+  console.log("Number of duplicate stops ommitted: " + duplicateCounter);
   return aggregator;
 };
 
@@ -37,32 +33,28 @@ function returnTagValues(array) {
   return array.map(function(entry) { return entry.tag; });
 }
 
-function getRouteStopPoints(routeTag, routeAggregator, stopAggregator, writeDir) {
-  console.log(routeTag);
-  //var routeSchedule = readJSON(`${writeDir}/${routeTag}.json`)
-  var routeSchedule = readJSON(dir+'/schedules/'+routeTag+'.json')
+function getRouteStopPoints(routeData, routeAggregator, stopAggregator, writeDir) {
   try {
-    var routeJson = readJSON(writeDir+'/'+routeTag+'.json', "utf8");
-    stopAggregator = addNewStops(routeJson.route.stop, stopAggregator, writeDir);
-    routeAggregator[routeJson.route.tag] = {
-      title: routeJson.route.title,
-      color: routeJson.route.color,
-      tag: routeJson.route.tag,
-      oppositeColor: routeJson.route.oppositeColor,
-      max: parseCoordinates([routeJson.route.latMax, routeJson.route.lonMax]),
-      min: parseCoordinates([routeJson.route.latMin, routeJson.route.lonMin]),
-      scheduleStops: {
-        inbound: routeSchedule.route[0] ? returnTagValues(routeSchedule.route[0].header.stop) : '',
-        outbound: routeSchedule.route[1] ? returnTagValues(routeSchedule.route[1].header.stop) : ''
-      },
+    stopAggregator = addNewStops(routeData.route.stop, stopAggregator, writeDir);
+    routeAggregator[routeData.route.tag] = {
+      title: routeData.route.title,
+      color: routeData.route.color,
+      tag: routeData.route.tag,
+      oppositeColor: routeData.route.oppositeColor,
+      max: parseCoordinates([routeData.route.latMax, routeData.route.lonMax]),
+      min: parseCoordinates([routeData.route.latMin, routeData.route.lonMin]),
+      // scheduleStops: {
+      //   inbound: routeSchedule.route[0] ? returnTagValues(routeSchedule.route[0].header.stop) : undefined,
+      //   outbound: routeSchedule.route[1] ? returnTagValues(routeSchedule.route[1].header.stop) : undefined
+      // },
       stops: {
-        inbound: routeJson.route.direction[0] ? routeJson.route.direction[0].stop.map(function(stop) { return stop.tag }) : '',
-        outbound: routeJson.route.direction[1] ? routeJson.route.direction[1].stop.map(function(stop) { return stop.tag }) : '',
+        inbound: routeData.route.direction[0] ? routeData.route.direction[0].stop.map(function(stop) { return stop.tag }) : undefined,
+        outbound: routeData.route.direction[1] ? routeData.route.direction[1].stop.map(function(stop) { return stop.tag }) : undefined,
       },
     };
   } catch (err) {
     console.log(err)
-    console.log("No data found for route " + routeTag);
+    console.log("No data found for route ");
   }
   return {
     routes: routeAggregator,
@@ -70,18 +62,19 @@ function getRouteStopPoints(routeTag, routeAggregator, stopAggregator, writeDir)
   }
 };
 
+var dir = "src/output";
+var routeDataDir = dir + "/routeData";
+var aggregatedDir = dir + "/aggregatedData";
+
 var routesJson = readJSON(`${routeDataDir}/index.json`);
 var rTags = getRouteTags(routesJson.route);
 console.log("============" + rTags.length);
 
+var results = rTags.reduce(function(acc, tag) {
+  console.log(tag);
+  var routeJson = readJSON(routeDataDir+'/'+tag+'.json', "utf8");//todo, export logic
+  return getRouteStopPoints(routeJson, acc.routes, acc.stops, routeDataDir);
+}, { stops: {}, routes: {}});
 
-var stopPoints = {};
-var abbreviatedRoutes = {};
-var results = rTags.map(function(tag) {
-  var collected = getRouteStopPoints(tag, abbreviatedRoutes, stopPoints, routeDataDir);
-  stopPoints = collected.stops;
-  abbreviatedRoutes = collected.routes;
-});
-
-writeJSON(`${aggregatedDir}/stops.json`, stopPoints);
-writeJSON(`${aggregatedDir}/routes.json`, abbreviatedRoutes);
+writeJSON(`${aggregatedDir}/stops.json`, results.stops);
+writeJSON(`${aggregatedDir}/routes.json`, results.routes);
