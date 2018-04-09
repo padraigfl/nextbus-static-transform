16a644984cc60f4afebcb89c1ae8752630a41d93
ComeDownToUs <blues4ceausescu@gmail.com>
2018-04-10 00:59:39 +0100

    schedule aggregation appears to... sorta work?

--- src/buildAggregatedData.js
+++ src/buildAggregatedData.js
@@ -6,13 +6,25 @@ function parseCoordinates(coordinates) {
   return [parseFloat(coordinates[0]), parseFloat(coordinates[1])];
 }
 
+function addStopRoutes(routeTag, directions, aggregator) {
+  directions.forEach(function(direction) {
+    direction.stop.forEach(function(stop) {
+      if (!aggregator[stop.tag].routes[routeTag]){
+        aggregator[stop.tag].routes[routeTag] = {};
+      }
+      aggregator[stop.tag].routes[routeTag][direction.name] = true;
+    });
+  });
+  return aggregator;
+}
+
 function addNewStops(routeStops, aggregator) {
   routeStops = forceArray(routeStops);
 
   // var duplicateCounter = 0;
   routeStops.forEach(function(stop) {
     if (!aggregator[stop.tag]) {
-      aggregator[stop.tag] = stop;
+      aggregator[stop.tag] = Object.assign({ routes: {} }, stop);
     }
     // else {
     //   duplicateCounter += 1;
@@ -59,26 +71,76 @@ function addCopyright(routes, stops, copyright) {
   }
 }
 
-function addScheduleData(routeData, scheduleData, routeAggregator, stopAggregator) {
-  routeAggregator.stops = { // stops that are scheduled
-    inbound: routeData.route.direction[0] ?
-      routeData.route.direction[0].stop.map(function(stop) { return stop.tag; }) : undefined,
-    outbound: routeData.route.direction[1] ?
-      routeData.route.direction[1].stop.map(function(stop) { return stop.tag; }) : undefined,
-  };
-  if(!stopAggregator.routes) {
-    stopAggregator.routes = {
-      inbound: { wkd: {}, sat: {}, sun: {}},
-      outbound: { wkd: {}, sat: {}, sun: {}},
+function getFirstTime(times){
+  for(var i in times){
+    if (times[i].epochTime !== '-1') {
+      return times[i].content;
+    }
+  }
+  return 'fail';
+}
+
+function getRouteScheduleDetails(daySchedule){
+  if (daySchedule[0] && Array.isArray(daySchedule[0].stop)) {
+    return {
+      first: getFirstTime(daySchedule[0].stop),
+      last: getFirstTime(daySchedule[daySchedule.length - 1].stop),
     };
   }
+}
+
+function getStopTimes(circuits, idx) {
+  return circuits.map(function( circuit) {
+    return circuit.stop[idx].content;
+  }).filter(function(t){
+    if(t !== '--'){
+      return t;
+    }
+  });
+}
+
+function getTimes(idx, daySchedule) {
+  if (daySchedule[0] && Array.isArray(daySchedule[0].stop)) {
+    return getStopTimes(daySchedule, idx);
+  }
+}
+
+//this is way easier with ES6
+function addScheduleData(routeTag, scheduleData, routeAggregator, stopAggregator) {
+  if(!Array.isArray(scheduleData.route)) {
+    scheduleData.route = [scheduleData.route];
+  }
+  if(routeAggregator[routeTag]) {
+    routeAggregator[routeTag].scheduledStops = { // stops that are scheduled
+      inbound: Array.isArray(scheduleData.route) && scheduleData.route[0].direction ?
+        scheduleData.route[0].header.stop.map(function(stop) { return stop.tag; }) : undefined,
+      outbound: Array.isArray(scheduleData.route) && scheduleData.route.length > 1 && scheduleData.route[1].direction ?
+        scheduleData.route[1].header.stop.map(function(stop) { return stop.tag; }) : undefined,
+    };
+  }
+
   scheduleData.route.forEach(function(day) {
     day.header.stop.forEach(function(stop, idx) { // add list of routes to stop
-      stopAggregator[stop.tag]
-        .routes[scheduleData.route][day.direction][day.serviceClass] = day.tr.stop.map(function(times) {
-          return times[idx];
-        });
+      if(!stopAggregator[stop.tag]){
+        return;
+      }
+      if(!stopAggregator[stop.tag].scheduleRoutes) {
+        stopAggregator[stop.tag].scheduleRoutes = {};
+      }
+      if(!stopAggregator[stop.tag].scheduleRoutes[routeTag] ) {
+        stopAggregator[stop.tag].scheduleRoutes[routeTag] = { };
+      }
+      if (!stopAggregator[stop.tag].scheduleRoutes[routeTag][day.direction]) {
+        stopAggregator[stop.tag].scheduleRoutes[routeTag][day.direction] = {};
+      }
+      stopAggregator[stop.tag].scheduleRoutes[routeTag][day.direction][day.serviceClass] = getTimes(idx, day.tr);
     });
+    if(routeAggregator[routeTag]){
+      if(routeAggregator[routeTag].scheduledStops && !routeAggregator[routeTag].scheduledStops[day.direction]){
+        routeAggregator[routeTag].scheduledStops[day.direction] = {};
+      }
+      routeAggregator[routeTag].scheduledStops[day.direction][day.serviceClass] = getRouteScheduleDetails(day.tr); // this makes more sense in routes
+    }
   });
   return {
     routes: routeAggregator,
@@ -96,6 +158,7 @@ function minifyRouteStopData(routeData, routeAggregator, stopAggregator) {
   addCopyright(routeAggregator, stopAggregator, routeData.copyright);
   try {
     stopAggregator.data = addNewStops(routeData.route.stop, stopAggregator.data);
+    stopAggregator.data = addStopRoutes(routeData.route.tag, routeData.route.direction, stopAggregator.data);
     routeAggregator.data[routeData.route.tag] = {
       title: routeData.route.title,
       color: routeData.route.color,
---
+++ src/example.js
@@ -0,0 +1,103 @@
+'use strict';
+
+var api = require('./apiFetch');
+var geo = require('./buildGeoJsons');
+var minifyRoutes = require('./buildAggregatedData');
+var io = require('./jsonIO');
+var writeJSON = io.writeJSON;
+var readJSON = io.readJSON;
+var dir = 'src/output';
+
+function getRoute(agency, routeTag) {
+  api.getRoute('sf-muni', routeTag)
+    .then( function(data) {
+      writeJSON(dir+'/routeData/'+routeTag+'.json', data);
+    })
+    .catch( function(err) {
+      console.log(err); // eslint-disable-line
+    } );
+}
+
+function getRouteSchedule(agency, routeTag) {
+  console.log('schedule')
+  api.getRouteSchedule('sf-muni', routeTag)
+    .then( function(data) {
+      writeJSON(dir+'/schedules/'+routeTag+'.json', data);
+    })
+    .catch( function(err) {
+      console.log(err); // eslint-disable-line
+    });
+}
+
+// /* Step 1: get files */
+// api.getRoutesTags('sf-muni')
+//   .then(function(data) {
+//     writeJSON(dir+'/routeData/index.json', data);
+//     data.route.map(function(route, i) {
+//       console.log(route.tag); // eslint-disable-line
+//       setTimeout(function() {
+//         getRoute('sf-muni', route.tag);
+//         getRouteSchedule('sf-muni', route.tag);
+//       }, i*2000);
+//     });
+//   });
+
+/* Step 2: Aggregate data */
+var routeDataDir = dir + '/routeData';
+var scheduleDataDir = dir + '/schedules';
+var aggregatedDir = dir + '/aggregatedData';
+var routesJson = readJSON(routeDataDir+'/index.json');
+var rTags = routesJson.route.map(function(route) {
+  console.log(route)
+  return route.tag; });
+var results = rTags.reduce(function(acc, tag) {
+  console.log(tag);
+  var routeJson = readJSON(routeDataDir+'/'+tag+'.json', 'utf8');//todo, export logic
+  return minifyRoutes(routeJson, acc.routes, acc.stops);
+}, { stops: {}, routes: {}});
+
+results = rTags.reduce(function(acc, tag) {
+  console.log(tag);
+  var routeJson = readJSON(routeDataDir+'/'+tag+'.json', 'utf8');
+  var scheduleJson = readJSON(scheduleDataDir+'/'+tag+'.json', 'utf8');//todo, export logic
+  if(scheduleJson){
+    return minifyRoutes.addScheduleData(tag, scheduleJson, acc.routes, acc.stops);
+  }
+  return acc;
+}, results);
+
+writeJSON(aggregatedDir+'/stops.json', results.stops);
+writeJSON(aggregatedDir+'/routes.json', results.routes);
+
+
+/* Step 3: build geojsons */
+// var aggregatedStops = readJSON(dir+'/aggregatedData/stops.json');
+// var routesJson = readJSON(dir+'/routeData/index.json');
+// function buildGeoRoutes(routes, geoObj) {
+//   if (!geoObj) {
+//     geoObj = { type: 'FeatureCollection', features: [] };
+//   }
+//   var routePaths = { type: 'FeatureCollection', features: [] };
+//   routes.forEach(function(route) {
+//     // TODO: replace with API req
+//     var routeJson = readJSON(dir+'/routeData/'+route+'.json');
+//     geoObj.features.push(geo.buildRoute(routeJson, routePaths));
+//   });
+//   return geoObj;
+// }
+// writeJSON(dir+'/geoJSONS/routes.json', buildGeoRoutes(
+//   routesJson.route.map(
+//     function (route) {
+//       return route.tag;
+//     }
+//   )
+// ));
+// writeJSON(
+//   dir+'/geoJSONS/stops.json',
+//   geo.buildFeaturesShell().features.push(
+//     Object.keys(aggregatedStops).map(function(stopKey) {
+//       return geo.buildStopPoint(aggregatedStops[stopKey]);
+//     })
+//   ),
+// );
+
